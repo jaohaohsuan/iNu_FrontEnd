abb61cc352eccd695539659f868e62060f837bf1
larryyangsen <larryyangsen@hotmail.com>
2015-05-28 17:02:44 +0800

    Follow John Papa style

--- js/controllers/main.js
+++ js/controllers/main.js
@@ -3,10 +3,24 @@
     var scriptPath = scripts[scripts.length - 1].src;
     var theScriptDirectory = scriptPath.substring(0, scriptPath.lastIndexOf('/'));
     angular.module('iNu')
-        .controller('mainController', ['$state', 'jsonMethodService', function ($state, jsonMethodService) {
-            var self = this;
-            self.theScriptDirectory = theScriptDirectory;
+        .controller('mainController', ['$state', 'jsonMethodService', mainController])
+
+
+    function mainController($state, jsonMethodService) {
+        var self = this;
+
+        self.logout =logout;
+        self.selectGroup = selectGroup;
 //            $state.go('main.crossAnalysis');
+        self.theScriptDirectory = theScriptDirectory;
+
+
+
+        doService();
+
+
+        function doService() {
+
             jsonMethodService.getJson('json/pagetitle.json').then(
                 function (data) {//success
                     self.userInfo = data;
@@ -14,14 +28,13 @@
 
                 }
             );
+        }
+        function logout(){
+            $state.go('login');
+        }
+        function selectGroup(item) {
+            console.log("selected:" + item);
+        };
 
-            self.selectGroup = function (item) {
-                console.log("selected:" + item);
-            };
-            self.logout = function () {
-                $state.go('login');
-            };
-
-        }])
-
+    }
 })();
