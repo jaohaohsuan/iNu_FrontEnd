e7642a144f81642a3a5131b43c457abdc0a7d1a2
larryyangsen <larryyangsen@hotmail.com>
2015-05-28 17:10:14 +0800

    Follow John Papa style

--- js/controllers/login.js
+++ js/controllers/login.js
@@ -3,23 +3,27 @@
  */
 (function(){
     angular.module('iNu')
-        .controller('loginController',['$state',function($state){
-            var self = this;
-            self.login = function(){
-                var account = self.account;
-                var password = self.password;
-                if (account == 'admin' && password == 'admin')
-                {
-                    console.log('Login')
-                    $state.go('main');
-                    self.errorMsg = null;
-                }
-                else
-                {
-                    self.account = '';
-                    self.password = '';
-                    self.errorMsg = "帳號或密碼錯誤";
-                }
+        .controller('loginController',['$state',loginController])
+
+    function loginController($state){
+        var self = this;
+        self.login =login;
+
+        function login(){
+            var account = self.account;
+            var password = self.password;
+            if (account == 'admin' && password == 'admin')
+            {
+                console.log('Login')
+                $state.go('main');
+                self.errorMsg = null;
             }
-        }])
+            else
+            {
+                self.account = '';
+                self.password = '';
+                self.errorMsg = "帳號或密碼錯誤";
+            }
+        }
+    }
 })()
--- js/controllers/recordList.js
+++ js/controllers/recordList.js
@@ -1,38 +1,32 @@
 (function () {
     angular.module('iNu')
-        .controller('recordListController', ['jsonMethodService','$scope',function (jsonMethodService,$scope) {
-            var self = this;
-            self.perItemHeight = 32;
-            self.maxExpandedHeight = 0;//grid擴展最大高度
-            jsonMethodService.getJson('json/callList.json').then(
-                function (data) {//success
-                    $scope.callListData = data
-                }, function (data) {//error
-                }
-            );
+        .controller('recordListController', ['jsonMethodService', '$scope', recordListController])
 
-            self.listOptions = {
-                enableSorting: true,
-                enableRowSelection: true,
-                enableRowHeaderSelection: false,
-                expandableRowTemplate: 'views/recordListGrdTemplate.html',
-                expandableRowHeight: 160,
-                expandableRowScope: {
-                    subGridVariable: 'subGridScopeVariable'
-                },
-                multiSelect : false,
-                columnDefs: [
-                    { name: '任務流水號', field: 'taskFlow'},
-                    {"name": '業務模型', field: 'model',cellTemplate: '<span>自訂樣板</span>'},
-                    {"name": '通話時長(秒)', field: 'callRange'},
-                    {"name": '靜音時長(秒)',field: 'muteRange'},
-                    {"name": '語速',field: 'callSpeed'},
-                    {"name": '靜音點比',field: 'mutePercentage'},
-                    {"name": '座席工號',field: 'sitNumber'},
-                    {"name": '座席班組',field: 'sitGroup'},
-                    {"name": '通話時間',field: 'callTime'},
-                    {"name": '受理號碼',field: 'acceptedNumber'},
-                    {"name": '來電原因',field: 'callReason'}
+    function recordListController(jsonMethodService, $scope) {
+        var self = this;
+        self.perItemHeight = 32;
+        self.listOptions = {
+            enableSorting: true,
+            enableRowSelection: true,
+            enableRowHeaderSelection: false,
+            expandableRowTemplate: 'views/recordListGrdTemplate.html',
+            expandableRowHeight: 160,
+            expandableRowScope: {
+                subGridVariable: 'subGridScopeVariable'
+            },
+            multiSelect: false,
+            columnDefs: [
+                {name: '任務流水號', field: 'taskFlow'},
+                {"name": '業務模型', field: 'model', cellTemplate: '<span>自訂樣板</span>'},
+                {"name": '通話時長(秒)', field: 'callRange'},
+                {"name": '靜音時長(秒)', field: 'muteRange'},
+                {"name": '語速', field: 'callSpeed'},
+                {"name": '靜音點比', field: 'mutePercentage'},
+                {"name": '座席工號', field: 'sitNumber'},
+                {"name": '座席班組', field: 'sitGroup'},
+                {"name": '通話時間', field: 'callTime'},
+                {"name": '受理號碼', field: 'acceptedNumber'},
+                {"name": '來電原因', field: 'callReason'}
 //                    {"name": '用戶品牌',field: 'userBrand'},
 //                    {"name": '客戶等級',field: 'customerRating'},
 //                    {"name": '滿意度',field: 'satisfaction'},
@@ -41,11 +35,21 @@
 //                    {"name": '客服班組',field: 'agentTeamNumber'},
 //                    {"name": '數據源',field: 'source'}
 
-                ],
-                data: 'callListData',
+            ],
+            data: 'callListData',
 
-            };
+        };
+        self.maxExpandedHeight = 0;//grid擴展最大高度
 
 
-        }])
+        doService();
+        function doService() {
+            jsonMethodService.getJson('json/callList.json').then(
+                function (data) {//success
+                    $scope.callListData = data
+                }, function (data) {//error
+                }
+            );
+        }
+    }
 })()
