5932f085f362f4719f8b4dc099c6ad984ec37a78
larryyangsen <larryyangsen@hotmail.com>
2015-06-03 15:28:48 +0800

    因ie使用設定上有問題，先讓IE不會跑出齒輪

--- js/inspinia.js
+++ js/inspinia.js
@@ -9,7 +9,10 @@ $(document).ready(function () {
 
     // Append config box / Only for demo purpose
     $.get("views/skin-config.html", function (data) {
-        $('body').append(data);
+
+        var IEversion = detectIE();
+        if (!IEversion !== false)
+            $('body').append(data);
     });
 
     // Full height of sidebar
@@ -20,26 +23,26 @@ $(document).ready(function () {
         var navbarHeigh = $('nav.navbar-default').height();
         var wrapperHeigh = $('#page-wrapper').height();
 
-        if(navbarHeigh > wrapperHeigh){
+        if (navbarHeigh > wrapperHeigh) {
             $('#page-wrapper').css("min-height", navbarHeigh + "px");
         }
 
-        if(navbarHeigh < wrapperHeigh){
-            $('#page-wrapper').css("min-height", $(window).height()  + "px");
+        if (navbarHeigh < wrapperHeigh) {
+            $('#page-wrapper').css("min-height", $(window).height() + "px");
         }
 
     }
 
 
-    $(window).bind("load resize scroll", function() {
-        if(!$("body").hasClass('body-small')) {
+    $(window).bind("load resize scroll", function () {
+        if (!$("body").hasClass('body-small')) {
             fix_height();
         }
     })
 
     // Move right sidebar top after scroll
-    $(window).scroll(function(){
-        if ($(window).scrollTop() > 0 && !$('body').hasClass('fixed-nav') ) {
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 0 && !$('body').hasClass('fixed-nav')) {
             $('#right-sidebar').addClass('sidebar-top');
         } else {
             $('#right-sidebar').removeClass('sidebar-top');
@@ -47,14 +50,14 @@ $(document).ready(function () {
     });
 
 
-    setTimeout(function(){
+    setTimeout(function () {
         fix_height();
     })
 });
 
 // Minimalize menu when screen is less than 768px
-$(function() {
-    $(window).bind("load resize", function() {
+$(function () {
+    $(window).bind("load resize", function () {
         if ($(this).width() < 769) {
             $('body').addClass('body-small')
         } else {
@@ -62,3 +65,38 @@ $(function() {
         }
     })
 })
+
+
+function detectIE() {
+    var ua = window.navigator.userAgent;
+
+    // test values
+    // IE 10
+    //ua = 'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Trident/6.0)';
+    // IE 11
+    //ua = 'Mozilla/5.0 (Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko';
+    // IE 12 / Spartan
+    //ua = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.71 Safari/537.36 Edge/12.0';
+
+    var msie = ua.indexOf('MSIE ');
+    if (msie > 0) {
+        // IE 10 or older => return version number
+        return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
+    }
+
+    var trident = ua.indexOf('Trident/');
+    if (trident > 0) {
+        // IE 11 => return version number
+        var rv = ua.indexOf('rv:');
+        return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+    }
+
+    var edge = ua.indexOf('Edge/');
+    if (edge > 0) {
+        // IE 12 => return version number
+        return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
+    }
+
+    // other browser
+    return false;
+}
